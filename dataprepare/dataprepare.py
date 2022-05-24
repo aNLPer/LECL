8d935d10e946986841b6059529b978ddce46b61b
xuqi <welch_cn@163.com>
2022-05-24 08:02:09 +0800

    数据预处理

--- dataprepare/dataprepare.py
+++ dataprepare/dataprepare.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import json
+import jieba
+import thulac
 import utils.commonUtils as utils
 
 
@@ -8,6 +10,93 @@ import utils.commonUtils as utils
 def stopwordslist(filepath):
     stopwords = [line.strip() for line in open(filepath, 'r', encoding='utf-8').readlines()]
     return stopwords
+
+# 大写数字转阿拉伯数字
+def hanzi_to_num(hanzi_1):
+    # for num<10000
+    hanzi = hanzi_1.strip().replace('零', '')
+    if hanzi == '':
+        return str(int(0))
+    d = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '': 0}
+    m = {'十': 1e1, '百': 1e2, '千': 1e3, }
+    w = {'万': 1e4, '亿': 1e8}
+    res = 0
+    tmp = 0
+    thou = 0
+    for i in hanzi:
+        if i not in d.keys() and i not in m.keys() and i not in w.keys():
+            return hanzi
+
+    if (hanzi[0]) == '十': hanzi = '一' + hanzi
+    for i in range(len(hanzi)):
+        if hanzi[i] in d:
+            tmp += d[hanzi[i]]
+        elif hanzi[i] in m:
+            tmp *= m[hanzi[i]]
+            res += tmp
+            tmp = 0
+        else:
+            thou += (res + tmp) * w[hanzi[i]]
+            tmp = 0
+            res = 0
+    return int(thou + res + tmp)
+
+def get_cutter(dict_path="Thuocl_seg.txt", mode='thulac', stop_words_filtered=False):
+    '''
+    获取分词器
+    :param dict_path: jieba、thulac使用的用户字典
+    :param mode: 分词工具选择
+    :param stop_words_filtered: 停用词过滤
+    :return:
+    '''
+    if stop_words_filtered:
+        stopwords = stopwordslist('law_processed/stop_word.txt')  # 这里加载停用词的路径
+    else:
+        stopwords = []
+    if mode == 'jieba':
+        jieba.load_userdict(dict_path)
+        return lambda x: [a for a in list(jieba.cut(x)) if a not in stopwords]
+    elif mode == 'thulac':
+        thu = thulac.thulac(user_dict=dict_path, seg_only=True)
+        return lambda x: [a for a in thu.cut(x, text=True).split(' ') if a not in stopwords]
+
+def process_law(law, cut):
+    # single article
+    # cut=get_cutter()
+    condition_list = []
+    for each in law.split('。')[:-1]:
+        suffix = None
+        if '：' in each:
+            each, suffix = each.split('：')
+            suffix = cut(suffix)
+        words = cut(each)
+        seg_point = [-1]
+        conditions = []
+
+        for i in range(len(words)):
+            if words[i] == '；' or words[i] == ';':
+                seg_point.append(i)
+        seg_point.append(len(words))
+        for i in range(len(seg_point) - 1):
+            for j in range(seg_point[i + 1] - 1, seg_point[i], -1):
+                if j + 1 < len(words) and words[j] == '的' and words[j + 1] == '，':
+                    conditions.append(words[seg_point[i] + 1:j + 1])
+                    break
+        # context=law.split('。')[:-1]
+        for i in range(1, len(conditions)):
+            conditions[i] = conditions[0] + conditions[i]
+        # if len(condition_list)==0 and len(conditions)==0:
+        #     conditions.append([])
+        if suffix is not None:
+            conditions = [x + suffix for x in conditions]
+        condition_list += conditions
+
+    if condition_list == []:
+        condition_list.append(cut(law[:-1]))
+    n_word = [len(i) for i in condition_list]
+    return condition_list, n_word
+
+
 
 # 生成acc2desc字典
 def get_acc_desc(file_path):
@@ -18,8 +107,6 @@ def get_acc_desc(file_path):
             if dict["accusation"] not in acc2des:
                 acc2des[dict["accusation"]] = dict["desc"]
     return acc2des
-acc2desc = get_acc_desc("accusation_description.json")
-print(acc2desc)
 
 # 构造数据集
 def getData(case_path, acc2desc):
@@ -38,6 +125,10 @@ def getData(case_path, acc2desc):
             item.append(example['meta']['accusation'])
             item.append(acc2desc[example['meta']['accusation']])
             items.append(item)
+
+
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,2 @@
+print("sdfas。safasf。啊书法大赛。".split('。'))
+print("sdfaf"[:-1])
